c1f81691326a9a35d00ebbd08561d6fa401ef350
soufianeBenRahim <32791470+soufianeBenRahim@users.noreply.github.com>
2022-01-22 12:14:29 +0100

    Creation dela couche infrastrecture specifique a l'application WPF Configuration de projet de test pour .net5

---
+++ clean Architecture (Soudiane)/src/POS_WPF/Infrastrecture/Persistence/Configurations/CatalogEntityConfigurations/CatalogBrandEntityTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Clean_Architecture_Soufiane.Domain.AggregatesModel.Catalog;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Clean_Architecture_Soufiane.Infrastructure.Persistence.Configurations
+{
+    class CatalogBrandEntityTypeConfiguration
+        : IEntityTypeConfiguration<CatalogBrand>
+    {
+        public void Configure(EntityTypeBuilder<CatalogBrand> builder)
+        {
+            builder.ToTable("CatalogBrand");
+
+            builder.HasKey(ci => ci.Id);
+
+            builder.Property(ci => ci.Id)
+               .UseHiLo("catalog_brand_hilo")
+               .IsRequired();
+
+            builder.Property(cb => cb.Brand)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
