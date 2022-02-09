a6a602a7ddf72dc35f85b45028274394db9065ec
soufianeBenRahim <32791470+soufianeBenRahim@users.noreply.github.com>
2022-02-09 15:44:50 +0100

    Mise en marche de system de l'injection des depandances pour le projet POS et les testes

--- clean Architecture (Soudiane)/src/POS_WPF/UI/App.xaml.cs
+++ clean Architecture (Soudiane)/src/POS_WPF/UI/App.xaml.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Clean_Architecture_Soufiane.Infrastructure.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -22,6 +23,9 @@ using POS.ViewModel;
 using Clean_Architecture_Soufiane.Domain.AggregatesModel.Identity;
 using WPFLocalizeExtension.Engine;
 using System.Globalization;
+using Microsoft.Extensions.Hosting;
+using POS.Navigation;
+using Microsoft.AspNetCore.Hosting;
 
 namespace POS
 {
@@ -30,18 +34,64 @@ namespace POS
     /// </summary>
     public partial class App : Application
     {
-
+        private readonly  IHost _host;
         public App()
         {
             LocalizeDictionary.Instance.Culture = CultureInfo.CurrentCulture;
-            ConfigurationService.GetInstance(false);
+            _host = Host.CreateDefaultBuilder()
+              .ConfigureLogging(builder => {
+                  builder.ClearProviders();
+                  builder.AddDebug();
+              })            
+              .ConfigureServices(services =>
+              {
+                  ConfigurationService.GetInstance(services, false);
+                
+              })
+              .Build();
         }
-     
+
+        private static void DataBaseSeed()
+        {
+            try
+            {
+                IDbContextFactory<ApplicationDbContext> dbFactory = ConfigurationService.getService<IDbContextFactory<ApplicationDbContext>>();
+        var SaleContext = dbFactory.CreateDbContext();
+
+                if (SaleContext.Database.IsSqlite())
+                {
+                    SaleContext.Database.EnsureDeleted();
+                    SaleContext.Database.EnsureCreated();
+                }
+           
+                
+
+                var logger =ConfigurationService.getService<ILogger<ApplicationDbContextSeed>>();
+
+                var loggerSales = ConfigurationService.getService<ILogger<ApplicationDbContextSeed>>();
+                new ApplicationDbContextSeed().SeedAsync(SaleContext, logger)
+                                                   .Wait();
+
+            }
+            catch (Exception ex)
+            {
+                var logger = ConfigurationService.getService<ILogger<App>>();
+
+                logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+
+                throw;
+            }
+        }
         private void OnStartup(object sender, StartupEventArgs e)
         {
-            var mainWindow = ConfigurationService.getService<MainPage>();
-            mainWindow.Show();
+
+            _host.Start();
+            DataBaseSeed();
+            MainWindow = ConfigurationService.getService<MainPage>();
+            MainWindow.Show();
+
         }
+      
     }
 
 }
--- clean Architecture (Soudiane)/src/POS_WPF/UI/ConfigurationService.cs
+++ clean Architecture (Soudiane)/src/POS_WPF/UI/ConfigurationService.cs
@@ -1,15 +1,23 @@
-﻿using Clean_Architecture_Soufiane.Application.Common.Interfaces;
+﻿using Clean_Architecture_Soufiane.Application;
+using Clean_Architecture_Soufiane.Application.Common.Interfaces;
 using Clean_Architecture_Soufiane.Domain.AggregatesModel.Identity;
 using Clean_Architecture_Soufiane.Domain.AggregatesModel.Sales;
+using Clean_Architecture_Soufiane.Infrastructure;
 using Clean_Architecture_Soufiane.Infrastructure.Persistence;
 using Clean_Architecture_Soufiane.Infrastructure.Repositories;
 using Clean_Architecture_Soufiane.Infrastructure.Services;
 using DeskTop.Integration.Tests;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using POS.Navigation;
 using POS.View;
 using POS.ViewModel;
 using System;
+using System.IO;
 using System.Windows;
 
 namespace POS.Services
@@ -17,16 +25,12 @@ namespace POS.Services
     public class ConfigurationService 
     {
         private static  ServiceProvider serviceProvider;
-        public ConfigurationService()
-        {
 
-        }
-        public static void GetInstance(bool isMock)
+        public static void GetInstance(IServiceCollection services,bool isMock)
         {
             if (serviceProvider == null)
             {
-                ServiceCollection services = new ServiceCollection();
-                ConfigureServices(services, isMock);
+                ConfigureServices(services,isMock);
                 serviceProvider = services.BuildServiceProvider();
             }
         }
@@ -34,34 +38,36 @@ namespace POS.Services
         {
             return serviceProvider.GetService<T>();
         }
-        private static  void ConfigureServices(ServiceCollection services, bool isMok)
+     
+        private static  void ConfigureServices(IServiceCollection services,bool isMock)
         {
-            if (isMok)
+            if (isMock)
             {
+                services.AddLogging();
                 services.AddTransient<IDateTime, FakeDateTimeServcie>();
                 services.AddSingleton<IUsersRepository, FakeUsersRepository>();
                 services.AddSingleton<INavigationService, FakeNavigationProxy>();
+
+                services.AddSingleton<LoginViewModel>();
+                services.AddSingleton<LoginForm>();
+                services.AddSingleton<MainPage>();
             }
             else
             {
-                services.AddDbContext<ApplicationDbContext>(options =>
-                {
-                    options.UseSqlite("Data Source = SaleDataBase.db");
-                });
+                services.AddDbContextFactory<ApplicationDbContext>(opt =>
+                  opt.UseSqlite("Data Source = SaleDataBase.db"));
 
+                services.AddInfrastructure();
+                services.AddApplication();
                 // infrastrecture dependancy injection
-                services.AddScoped<IDomainEventService, DomainEventService>();
-                services.AddTransient<IDateTime, DateTimeService>();
-                services.AddScoped<ISaleRepository, SaleRepository>();
-
                 services.AddSingleton<ICurrentUserService, CurrentUserServiceWPF>();
-                services.AddSingleton<IDateTime, DateTimeService>();
-                services.AddSingleton<IUsersRepository, UsersRepository>();
+                services.AddSingleton<INavigationService, NavigationProxy>();
+                services.AddSingleton<LoginViewModel>();
+                services.AddSingleton<LoginForm>();
+                services.AddSingleton<MainPage>();
             }
-
-            services.AddSingleton<LoginViewModel>();
-            services.AddSingleton<LoginForm>();
-            services.AddSingleton<MainPage>();
+     
+            
         }
      
     }
