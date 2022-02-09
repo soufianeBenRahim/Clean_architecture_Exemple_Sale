07cdab6e4fa27ca74a7c77a9522eeaa2523bb2e3
soufianeBenRahim <32791470+soufianeBenRahim@users.noreply.github.com>
2022-02-09 15:38:09 +0100

    ajouter le evente handler SaleStatusChangedToPaidCachDomainEventHandler au projet applicationWPF

---
+++ clean Architecture (Soudiane)/src/POS_WPF/ApplicationWPF/Sale/DomainEventHandlers/SaleStatusChangedToPaidCachDomainEventHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace Clean_Architecture_Soufiane.Application.DomainEventHandlers
+{
+    using Clean_Architecture_Soufiane.Application.Common.Models;
+
+    using Domain.Events;
+    using MediatR;
+ 
+    using Microsoft.Extensions.Logging;
+
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class SaleStatusChangedToPaidCachDomainEventHandler : INotificationHandler<DomainEventNotification<SaleStatusChangedToPaidCachDomainEvent>>
+    {
+        private readonly ILogger<SaleStatusChangedToPaidCachDomainEvent> _logger;
+
+        public SaleStatusChangedToPaidCachDomainEventHandler(ILogger<SaleStatusChangedToPaidCachDomainEvent> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Handle(DomainEventNotification<SaleStatusChangedToPaidCachDomainEvent> notification, CancellationToken cancellationToken)
+        {
+            var domainEvent = notification.DomainEvent;
+            
+            _logger.LogInformation("Clean_Architecture_Soufiane Domain Event: {DomainEvent}", domainEvent.GetType().Name);
+
+
+            // TO DO Add traitement
+            return Task.CompletedTask;
+        }
+    }
+}
