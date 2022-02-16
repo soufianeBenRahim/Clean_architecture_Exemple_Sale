fbb9eaa0b84e76eec7209ad5aefe6a77d5fa54a5
Rahim soufiane <32791470+soufianeBenRahim@users.noreply.github.com>
2022-02-16 21:47:44 +0100

    Test ConfigurationService

---
+++ clean Architecture (Soudiane)/tests/DeskTop/DeskTop.Unit.Tests/ConfigurationServiceUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using POS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskTop.Unit.Tests
+{
+    public  class ConfigurationServiceUnitTest
+    {
+        [Test]
+        public void ConfigurationService_WheneCallGetService_ShouldeReturneTheRegistredService()
+        {
+            var isMoke = true;
+            var inMemoryDatabase = true;
+            var services = new ServiceCollection();
+            services.AddLogging();
+            ConfigurationService.GetInstance(services, isMoke, inMemoryDatabase);
+            var Navigation = ConfigurationService.getService<INavigationService>();
+            Assert.IsNotNull(Navigation);
+        }
+    }
+}
