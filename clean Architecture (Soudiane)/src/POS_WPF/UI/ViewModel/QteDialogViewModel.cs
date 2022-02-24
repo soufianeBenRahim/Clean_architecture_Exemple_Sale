81281c98b8b27d3f2a71b0d19a9192c619c0804b
Rahim soufiane <32791470+soufianeBenRahim@users.noreply.github.com>
2022-02-24 12:39:21 +0100

    test of QteDialogViewModel

--- clean Architecture (Soudiane)/src/POS_WPF/UI/ViewModel/QteDialogViewModel.cs
+++ clean Architecture (Soudiane)/src/POS_WPF/UI/ViewModel/QteDialogViewModel.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace POS.ViewModel
 {
-    public class QteDialogViewModel: ViewModelBase
+    public class QteDialogViewModel : ViewModelBase
     {
+        private decimal _qte;
+        public decimal Qte { get=>_qte; 
+            set { _qte = value;OnPropertyChanged("Qte"); } }
+
+        public void Ok()
+        {
+            this.Result = Qte;
+            CurentView.CloseWindow();
+        }
     }
 }
---
+++ clean Architecture (Soudiane)/tests/DeskTop/DeskTop.Unit.Tests/QteDialogViewModelUnitTest.cs
@@ -0,0 +1,54 @@
+﻿using Clean_Architecture_Soufiane.Domain.AggregatesModel.Catalog;
+using Clean_Architecture_Soufiane.Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using POS;
+using POS.Services;
+using POS.Services.TesteServices;
+using POS.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskTop.Unit.Tests
+{
+    public class QteDialogViewModelUnitTest
+    {
+        [OneTimeSetUp]
+        public void setup()
+        {
+            var isMoke = true;
+            var inMemoryDatabase = true;
+            var services = new ServiceCollection();
+            services.AddLogging();
+            ConfigurationService.GetInstance(services, isMoke, inMemoryDatabase);
+        }
+        [Test]
+        public void QteDialogViewModel_WhenNonValidateDialog_ShouldReturnNull()
+        {
+            var viewModel = new QteDialogViewModel();
+            Assert.IsNull(viewModel.Result);
+        }
+
+        [Test]
+        public void QteDialogViewModel_WhenValidateDialog_ShouldCloseForme()
+        {
+            var viewModel = new QteDialogViewModel();
+            viewModel.SetView(new FakeView());
+            viewModel.Ok();
+            Assert.IsTrue(viewModel.isFormClosed());
+        }
+
+        [Test]
+        public void QteDialogViewModel_WhenValidateDialog_ShouldSetTheResultToQteProperty()
+        {
+            var viewModel = new QteDialogViewModel();
+            viewModel.SetView(new FakeView());
+            viewModel.Qte = 2m;
+            viewModel.Ok();
+            Assert.AreEqual(Convert.ToDecimal(viewModel.Result),2m);
+        }
+    }
+}
