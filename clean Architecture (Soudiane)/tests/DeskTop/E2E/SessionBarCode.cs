ab4a44380cd5c82c23e8929f3aab5b06472697d4
Rahim soufiane <32791470+soufianeBenRahim@users.noreply.github.com>
2022-02-27 15:03:49 +0100

    First commit of E2E test project

---
+++ clean Architecture (Soudiane)/tests/DeskTop/E2E/SessionBarCode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+
+namespace WPF_POS_Test
+{
+    [TestClass]
+    public  class SessionBarCode : WPFPOSSession
+    {
+        [TestMethod]
+        public void EditorEnterText()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+        }
+
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+    }
+}
---
+++ clean Architecture (Soudiane)/tests/DeskTop/E2E/WPFPOSSession.cs
@@ -0,0 +1,78 @@
+﻿//******************************************************************************
+//
+// Copyright (c) 2017 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium;
+using System;
+
+namespace WPF_POS_Test
+{
+    public class WPFPOSSession
+    {
+        protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string POSAppId = @"C:\Users\H O P E\source\repos\soufianeBenRahim\Clean_architecture_Exemple_Sale\clean Architecture (Soudiane)\src\POS_WPF\UI\bin\Debug\net5.0-windows\POS.exe";
+
+        protected static WindowsDriver<WindowsElement> session;
+
+        public static void Setup(TestContext context)
+        {
+            // Launch a new instance of POS application
+            if (session == null)
+            {
+                // Create a new session to launch POS application
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app", POSAppId);
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+                Assert.IsNotNull(session);
+                Assert.IsNotNull(session.SessionId);
+
+
+                // Set implicit timeout to 1.5 seconds to make element search to retry every 500 ms for at most three times
+                session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
+
+
+            }
+        }
+
+        public static void TearDown()
+        {
+            // Close the application and delete the session
+            if (session != null)
+            {
+                session.Close();
+
+                try
+                {
+                   
+                }
+                catch { }
+
+                session.Quit();
+                session = null;
+            }
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+
+        protected static string SanitizeBackslashes(string input) => input.Replace("\\", Keys.Alt + Keys.NumberPad9 + Keys.NumberPad2 + Keys.Alt);
+    }
+}
